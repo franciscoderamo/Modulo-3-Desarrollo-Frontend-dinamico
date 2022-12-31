f73c75c734535e12e3f3c8031b237c3291af60fe
franciscoderamo <franciscoderamo@gmail.com>
2022-12-31 04:02:48 -0300

    Remove features from slide portfolios

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -95,20 +95,6 @@ modalCloses.forEach((modalClose) => {
   });
 });
 
-/*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
-    cssMode: true,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
 
