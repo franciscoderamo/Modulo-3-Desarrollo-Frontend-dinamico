4a38b71187c18b60d0e71f8f13926e49a72747ec
franciscoderamo <franciscoderamo@gmail.com>
2023-01-06 23:08:54 -0300

    Update service modal windows

--- src/app/componentes/services/services.component.ts
+++ src/app/componentes/services/services.component.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/servicios/portfolio.service';
+//import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  styleUrls: ['./services.component.css'],
+  //providers: [NgbModalConfig, NgbModal]
 })
 export class ServicesComponent implements OnInit {
-  miPortfolio:any;
-  constructor(private datosPortfolio:PortfolioService) { }
+  servicesList:any;
+  constructor(private datosPortfolio:PortfolioService,config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+    config.centered = true;
+  }
 
   ngOnInit(): void {
     this.datosPortfolio.obtenerDatos().subscribe(data =>{
       console.log(data);
-      this.miPortfolio=data;
+      this.servicesList=data;
     });
   }
+
+  open(content: any) {
+    this.modalService.open(content);
+  }
+
+  // export class ServicesComponent implements OnInit {
+  //   miPortfolio:any;
+  //   constructor(private datosPortfolio:PortfolioService) { }
+
+  //   ngOnInit(): void {
+  //     this.datosPortfolio.obtenerDatos().subscribe(data =>{
+  //       console.log(data);
+  //       this.miPortfolio=data;
+  //     });
+  //   }
 
 }
